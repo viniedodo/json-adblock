900714169f90b4928af30f60a0803cb39d4de0cc
viniedodo <962419+viniedodo@users.noreply.github.com>
2019-11-28 15:44:38 +0800

    fix add index -= 1
    splice bug

--- xiaohongshu.js
+++ xiaohongshu.js
@@ -4,7 +4,20 @@ let body = $response.body
 body=JSON.parse(body)
 body['data'].forEach((element, index)=> {
     if(element['is_ads']=="1"){ 
-          body['data'].splice(index,1)  
+          body['data'].splice(index,1)
+          index -= 1
      } 
  })
 $done({body:JSON.stringify(obj)})
+
+/* 网上看的
+
+for (var i = 0; i < obj.data.length; i++) {
+    if (obj.data[i].is_adsis_ads == 1) {
+        obj.data.splice(i, 1)
+        i -= 1
+     }
+}
+
+
+*/
