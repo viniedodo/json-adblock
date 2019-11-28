98f031f18adff9a9c4fa67c85b7204764fddee58
viniedodo <962419+viniedodo@users.noreply.github.com>
2019-11-28 18:46:31 +0800

    Update xiaohongshu.js

--- xiaohongshu.js
+++ xiaohongshu.js
@@ -1,5 +1,5 @@
 // https:\/\/www\.xiaohongshu\.com\/api\/sns\/v6\/homefeed\/ url script-response-body https://raw.githubusercontent.com/viniedodo/kaishujianggushi/master/xiaohongshu.js?token=AAHK64ZY62AM5EQ7NXCCKYK53X7C2
-
+/*
 let body = $response.body
 body=JSON.parse(body)
 body['data'].forEach((element, index)=> {
@@ -10,8 +10,9 @@ body['data'].forEach((element, index)=> {
  })
 $done({body:JSON.stringify(obj)})
 
-/* 网上看的
+*/
 
+/* 网上看的
 for (var i = 0; i < obj.data.length; i++) {
     if (obj.data[i].is_adsis_ads == 1) {
         obj.data.splice(i, 1)
@@ -19,5 +20,14 @@ for (var i = 0; i < obj.data.length; i++) {
      }
 }
 
-
 */
+
+var url = $request.url;
+var body = $response.body;
+var obj = JSON.parse($response.body);
+obj['data'].forEach((element, index)=> {
+    if(element['is_ads']=="1"){ 
+          obj['data'].splice(index,1);
+     } 
+ })
+$done({body: JSON.stringify(obj)});
