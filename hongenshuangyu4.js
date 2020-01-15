6ae509d123032820204753c96c219fcd8eb80745
viniedodo <viniedodo@gmail.com>
2020-01-15 12:12:25 +0800

    Update hongenshuangyu4.js

--- hongenshuangyu4.js
+++ hongenshuangyu4.js
@@ -1,15 +1,11 @@
 let url = $request.url;
-let body = JSON.parse($response.body);
+let obj = JSON.parse($response.body);
 
 
-let purchase = '/v1/get_purchase_list/';
 
+obj.result.vip_status.vip_type = 1;
+body = JSON.stringify(obj);
 
-if (url.indexOf(purchase) != -1) {
-	let obj = JSON.parse(body);
-	obj.result.vip_status.vip_type = 1;
-	body = JSON.stringify(obj);
-}
 
 
 $done({body});
