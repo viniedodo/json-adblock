952ff3127df4eac0ef1181c699366c9806bd1fb2
viniedodo <viniedodo@gmail.com>
2020-01-15 12:07:50 +0800

    Update hongenshuangyu3.js

--- hongenshuangyu3.js
+++ hongenshuangyu3.js
@@ -8,7 +8,7 @@ let bookfree = '/v1/get_limited_free_book_list/';
 if (url.indexOf(bookfree) != -1) {
 	let obj = JSON.parse(body);
 	obj.result.today_end = 1672416000;	
-	obj = JSON.stringify(obj);
+	body = JSON.stringify(obj);
 } 
 
 
