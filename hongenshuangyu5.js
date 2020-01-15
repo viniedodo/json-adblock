c95953a8d69f81b6ea7881b150dd77f0f5b7a7ff
viniedodo <viniedodo@gmail.com>
2020-01-15 11:58:09 +0800

    Create hongenshuangyu5.js

---
+++ hongenshuangyu5.js
@@ -0,0 +1,15 @@
+let url = $request.url;
+let body = JSON.parse($response.body);
+
+
+let user = '/v1/get_user_info/';
+
+
+if (url.indexOf(user) != -1) {
+	let obj = JSON.parse(body);
+	body.result.userinfo.vip_status.vip_type = 1;
+	body = JSON.stringify(obj);
+}
+
+
+$done({body});
