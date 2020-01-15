c63b9ee1f8db3681244b5ed7f3503d98b6476aea
viniedodo <viniedodo@gmail.com>
2020-01-15 11:42:34 +0800

    Update hongenshuangyu.js

--- hongenshuangyu.js
+++ hongenshuangyu.js
@@ -3,7 +3,7 @@
 ^https://bookapi.ihuman.com/v* url script-response-body https://raw.githubusercontent.com/viniedodo/json-adblock/master/hongenshuangyu.js
 
 
-
+l
 */
 
 
@@ -18,25 +18,34 @@ let purchase = '/v1/get_purchase_list/';
 let user = '/v1/get_user_info/';
 
 if (url.indexOf(bookvip) != -1) {
+	let obj = JSON.parse(body);
 	body.result.books.is_vip = 0;
+	body = JSON.stringify(obj);
 }
 
-else if (url.indexOf(levelvip) != -1) {
+if (url.indexOf(levelvip) != -1) {
+	let obj = JSON.parse(body);
 	body.result.levels.books.is_vip = 0;
+	body = JSON.stringify(obj);
 }
 
-else if (url.indexOf(bookfree) != -1) {
+if (url.indexOf(bookfree) != -1) {
+	let obj = JSON.parse(body);
 	body.result.today_end = 1672416000;	
+	body = JSON.stringify(obj);
 } 
 
-else if (url.indexOf(purchase) != -1) {
+if (url.indexOf(purchase) != -1) {
+	let obj = JSON.parse(body);
 	body.result.vip_status.vip_type = 1;
+	body = JSON.stringify(obj);
 }
 
-else if (url.indexOf(user) != -1) {
+if (url.indexOf(user) != -1) {
+	let obj = JSON.parse(body);
 	body.result.userinfo.vip_status.vip_type = 1;
+	body = JSON.stringify(obj);
 }
 
-	body = JSON.stringify(body);
 
 $done({body});
