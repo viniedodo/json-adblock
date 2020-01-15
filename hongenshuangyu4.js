a8d6a5caefa9fea6c7896e6fdb3a9ace19bb9a99
viniedodo <viniedodo@gmail.com>
2020-01-15 12:03:24 +0800

    Update hongenshuangyu4.js

--- hongenshuangyu4.js
+++ hongenshuangyu4.js
@@ -7,8 +7,8 @@ let purchase = '/v1/get_purchase_list/';
 
 if (url.indexOf(purchase) != -1) {
 	let obj = JSON.parse(body);
-	body.result.vip_status.vip_type = 1;
-	body = JSON.stringify(obj);
+	obj.result.vip_status.vip_type = 1;
+	obj = JSON.stringify(obj);
 }
 
 
