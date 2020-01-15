19c90593cd68f46fd17c49e153602e7974b4e348
viniedodo <viniedodo@gmail.com>
2020-01-15 12:13:42 +0800

    Update hongenshuangyu1.js

--- hongenshuangyu1.js
+++ hongenshuangyu1.js
@@ -1,12 +1,10 @@
 let url = $request.url;
-let body = JSON.parse($response.body);
+let obj = JSON.parse($response.body);
 
-let bookvip = '/v3/get_book_info/';
 
-if (url.indexOf(bookvip) != -1) {
-	let obj = JSON.parse(body);
-	obj.result.books.is_vip = 0;
-	body = JSON.stringify(obj);
-}
+
+obj.result.books.is_vip = 0;
+body = JSON.stringify(obj);
+
 
 $done({body});
