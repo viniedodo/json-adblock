1ac20defed845cc138f0efaf516dfc41245e9b0f
viniedodo <viniedodo@gmail.com>
2020-01-15 11:56:01 +0800

    Create hongenshuangyu1.js

---
+++ hongenshuangyu1.js
@@ -0,0 +1,12 @@
+let url = $request.url;
+let body = JSON.parse($response.body);
+
+let bookvip = '/v3/get_book_info/';
+
+if (url.indexOf(bookvip) != -1) {
+	let obj = JSON.parse(body);
+	body.result.books.is_vip = 0;
+	body = JSON.stringify(obj);
+}
+
+$done({body});
