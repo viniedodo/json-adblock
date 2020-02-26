db3a72f533b4b9f3ba5c618e7d0a735ed1e35c21
viniedodo <viniedodo@gmail.com>
2020-02-26 18:18:14 +0800

    Update lingumi.js

--- lingumi.js
+++ lingumi.js
@@ -8,7 +8,7 @@ obj["subscription"] = [
       "expirationDate": "2030-12-27T06:47:13.360Z",
       "subscriptionType": "com.lingumi.lingumichina.lessons.level1and2",
       "subscriptionStore": "Annual Plan",
-      "numberOfTrialLessons": "999"
+      "numberOfTrialLessons": 999
     }
   ];
 $done({body: JSON.stringify(obj)});
