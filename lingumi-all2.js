1190b01da705dce3f73b0108e050633d9216bb22
viniedodo <viniedodo@gmail.com>
2020-02-26 13:19:26 +0000

    Create lingumi-all2.js

---
+++ lingumi-all2.js
@@ -0,0 +1,15 @@
+//https:\/\/api.lingumi\.com\.cn\/core-users\/userInfo\/appData url script-response-body lingumi.js
+
+var obj = JSON.parse($response.body);
+if(true)
+{
+obj.subscription = [
+    {
+      "expirationDate":"2030-12-27T06:47:13.360Z",
+      "subscriptionType":"com.lingumi.lingumichina.lessons.level1and2",
+      "subscriptionStore":"Annual Plan",
+      "numberOfTrialLessons":999
+    }
+  ];
+$done({body: JSON.stringify(obj)});
+}
