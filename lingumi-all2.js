7c27604197fdc6e1ba58f5dc330ccec35362d551
viniedodo <viniedodo@gmail.com>
2020-02-27 14:38:51 +0800

    Update lingumi-all2.js

--- lingumi-all2.js
+++ lingumi-all2.js
@@ -1,8 +1,6 @@
 //https:\/\/api.lingumi\.com\.cn\/core-users\/userInfo\/appData url script-response-body lingumi.js
 
 var obj = JSON.parse($response.body);
-if(true)
-{
 obj.subscription = [
     {
       "expirationDate": "2030-12-27T06:47:13.360Z",
@@ -12,4 +10,4 @@ obj.subscription = [
     }
   ];
 $done({body: JSON.stringify(obj)});
-}
+
