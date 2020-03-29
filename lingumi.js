a33918136e5be1e45eccb67361133dd8a5e65390
viniedodo <viniedodo@gmail.com>
2020-03-29 13:10:11 +0800

    Update lingumi.js

--- lingumi.js
+++ lingumi.js
@@ -1,5 +1,6 @@
 //https:\/\/api.lingumi\.com\.cn\/core-users\/userInfo\/appData url script-response-body lingumi.js
 
+/*
 var obj = JSON.parse($response.body);
 if($request.method=="GET")
 {
@@ -13,3 +14,17 @@ obj["subscription"] = [
   ];
 $done({body: JSON.stringify(obj)});
 }
+*/
+
+var body = $response.body;
+var url = $request.url;
+const path = "/core-users/userInfo/appData";
+let obj = JSON.parse(body);
+if (url.indexOf(path) != -1) {
+	obj.subscription.expirationDate = "2030-12-27T06:47:13.360Z";
+    obj.subscription.subscriptionType = "com.lingumi.lingumichina.lessons.level1and2";
+    obj.subscription.subscriptionStore = "Annual Plan";
+    obj.subscription.numberOfTrialLessons = 999;
+	body = JSON.stringify(obj);
+ }
+$done({body});
