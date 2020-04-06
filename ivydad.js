16780658c98ebfd780771be3c360b88b0b9d7bd2
Viniedodo <viniedodo@gmail.com>
2020-04-06 13:43:04 +0800

    qq

--- ivydad.js
+++ ivydad.js
@@ -25,20 +25,20 @@ if ($request.url.indexOf("/api/audios/resource/*") != -1) {
 */
 
 function re() {
-    // var body = $response.body;
-    body = JSON.parse($response.body);
+    var body = $response.body;
+    var obj = JSON.parse(body);
     if (arguments[0].includes("@")) {
         var regs = arguments[0].split("@");
         var strs = arguments[1].split("@");
         for (i = 0; i < regs.length; i++) {
             var reg = newRegExp(regs[i], "g");
-            body = body.replace(reg, strs[i]);
+            obj = obj.replace(reg, strs[i]);
         }
     } else {
         var reg = newRegExp(arguments[0], "g");
-        body = body.replace(reg, arguments[1]);
+        obj = obj.replace(reg, arguments[1]);
     }
-    body=JSON.stringify(body);
+    body=JSON.stringify(obj);
     $done({body});
 }
 
