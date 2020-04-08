94fe6658403bdc066a49dc20ba8d1656c07de8e7
viniedodo <viniedodo@gmail.com>
2020-04-08 16:03:57 +0800

    Update ivydad.js

--- ivydad.js
+++ ivydad.js
@@ -13,8 +13,10 @@ if (url.indexOf(user) != -1){
 re()	
 }
 */
-
+if ($request.url.indexOf("/api/audios/resource/*") != -1) {
 re('"type":\\w+@"is_unlocked":\\w+@"unlock_state":\w+@"has_access":\\w+@"is_bought":\\d+@"hasBought":\\w+@"is_listen":\\d+@"is_try":\\d+@"is_lock":\\d+@"is_limited":\\d+@"listen":\\d+@"course_class":\\w+@"access_type":\\w+@"type":\\w+@"vip":\\w+@"activity":\\w+@"start":\\w+@"has_started":\\d+@"is_authed":\\w+@"authed":\\w+', '"type":free@"is_unlocked":true@"unlock_state":all@"has_access":true@"is_bought":1@"hasBought":true@"is_listen":1@"is_try":1@"is_lock":1@"is_limited":0@"listen":1@"course_class":free@"access_type":free"type":free"vip":free@"activity":true@"start":true@"has_started":1@"is_authed":true@"authed":true')
+}
+// re('"type":\\w+@"is_unlocked":\\w+@"unlock_state":\w+@"has_access":\\w+@"is_bought":\\d+@"hasBought":\\w+@"is_listen":\\d+@"is_try":\\d+@"is_lock":\\d+@"is_limited":\\d+@"listen":\\d+@"course_class":\\w+@"access_type":\\w+@"type":\\w+@"vip":\\w+@"activity":\\w+@"start":\\w+@"has_started":\\d+@"is_authed":\\w+@"authed":\\w+', '"type":free@"is_unlocked":true@"unlock_state":all@"has_access":true@"is_bought":1@"hasBought":true@"is_listen":1@"is_try":1@"is_lock":1@"is_limited":0@"listen":1@"course_class":free@"access_type":free"type":free"vip":free@"activity":true@"start":true@"has_started":1@"is_authed":true@"authed":true')
 
 function re() {
     var body = $response.body;
