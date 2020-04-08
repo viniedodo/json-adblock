33fc185907cc44abd78cc0cf173a0bc06c0b29bb
viniedodo <viniedodo@gmail.com>
2020-04-08 12:54:18 +0800

    test

--- ivydad.js
+++ ivydad.js
@@ -28,25 +28,23 @@ if ($request.url.indexOf("/api/audios/resource/*") != -1) {
 //var url = $request.url;
 //const path = "/api/audios/resource/*";
 
-if ($request.url.indexOf("/api/audios/resource/book/getDetail") != -1) {
-    re('"has_access":\\d@"is_bought":\\d', '"has_access":1@"is_bought":1')
-}
-
+re('"has_access":\\d+@"is_bought":\\d+', '"has_access":1@"is_bought":1')
 function re() {
     var body = $response.body;
+	var obj = JSON.parse($response.body);
     if (arguments[0].includes("@")) {
         var regs = arguments[0].split("@");
         var strs = arguments[1].split("@");
         for (i = 0; i < regs.length; i++) {
             var reg = new RegExp(regs[i], "g");
-            body = body.replace(reg, strs[i]);
+            obj = obj.replace(reg, strs[i]);
         }
-    } else {
+    } 
+	else {
         var reg = new RegExp(arguments[0], "g");
-        body = body.replace(reg, arguments[1]);
+        obj = obj.replace(reg, arguments[1]);
     }
-    //body=JSON.stringify(obj);
-    $done(body);
+    $done({body: JSON.stringify(obj)});
 }
 
 
