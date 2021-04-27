14d6c08dcb5507a074fba75c0c260734e039fa35
viniedodo <viniedodo@gmail.com>
2021-04-27 12:47:01 +0800

    1

--- mokao.js
+++ mokao.js
@@ -2,7 +2,7 @@ var body = $response.body;
 var url = $request.url;
 const path = "/Api/V2/Topic/";
 let obj = JSON.parse(body);
-if (url.indexOf(path) != -1) {
+if (url.indexOf(path) !== -1) {
 	obj.info.test_num = 9999;
 	body = JSON.stringify(obj);
  }
